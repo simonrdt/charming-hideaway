5bf54df8e5a91026c41fb963c59a30f6331cace9
Simon Reinhardt <hello@simonreinhardt.com>
2019-10-11 13:55:17 +0200

    builc

--- build/javascripts/site.js
+++ build/javascripts/site.js
@@ -12,6 +12,14 @@ $(document).ready(function(){
       $("#dark").removeClass("u-block");
     }
   });
+
+   $('.panel-collapse').on('show.bs.collapse', function () {
+    $(this).siblings('.panel-heading').addClass('active');
+  });
+
+  $('.panel-collapse').on('hide.bs.collapse', function () {
+    $(this).siblings('.panel-heading').removeClass('active');
+  });
 });
 
 

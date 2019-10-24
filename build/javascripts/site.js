97b8df69343c78b784884f5883072358b376ba5c
Simon Reinhardt <hello@simonreinhardt.com>
2019-10-24 13:45:02 +0200

    build

--- build/javascripts/site.js
+++ build/javascripts/site.js
@@ -12,14 +12,6 @@ $(document).ready(function(){
       $("#dark").removeClass("u-block");
     }
   });
-
-   $('.panel-collapse').on('show.bs.collapse', function () {
-    $(this).siblings('.panel-heading').addClass('active');
-  });
-
-  $('.panel-collapse').on('hide.bs.collapse', function () {
-    $(this).siblings('.panel-heading').removeClass('active');
-  });
 });
 
 
